572614d2266ae02b4523105b05a227404cf198d7
alohaxdxd <132444939+alohaxdxd@users.noreply.github.com>
2023-05-10 17:13:24 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function food() {
+	document.getElementById('img1').style.display='block';
+    document.getElementById('img2').style.display='none';
+    document.getElementById('img3').style.display='none';
+}
+
+function drinks() {
+	document.getElementById('img1').style.display='none';
+    document.getElementById('img2').style.display='block';
+    document.getElementById('img3').style.display='none';
+}
+
+function takeaway() {
+	document.getElementById('img1').style.display='none';
+    document.getElementById('img2').style.display='none';
+    document.getElementById('img3').style.display='block';
+}
